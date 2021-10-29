43ced5a09f2d200e5a2f17e8d680d742cd509c1f
Henrique Coan de Bona Sartor <henriquecdb2@gmail.com>
2021-10-28 23:20:07 -0300

    Add files via upload

---
+++ Desafio/main.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+    github: 'henriquecoan',
+    youtube: 'henriquecoan',
+    facebook:  'henrique.coandebonasartor',
+    instagram: 'henriquecoann',
+    twitter: 'henriquesartor'
+}
+
+function changeSocialMediaLinks(){
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+}
+changeSocialMediaLinks()
+
+function getGithubProfile() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+    })
+}
+getGithubProfile()
+
